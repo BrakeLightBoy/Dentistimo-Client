000f9f45cdd7c70f9db6a7e8c0573671e7cafe29
noahma <noahma@student.chalmers.se>
2022-11-22 15:08:03 +0000

    Update MainPage.js

--- src/routes/MainPage.js
+++ src/routes/MainPage.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Appointment from "../components/Appointment";
+import Map from "../components/Map";
+import "./MainPage.css";
 
 export default function Home() {
     return (
       <div>
+        <Map zoom={10} center={{"lat":57.75,"lng":11.92}} />
         <Appointment />
         <Appointment />
         <Appointment />
