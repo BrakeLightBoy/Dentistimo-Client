a8558f5e73cd4f3639355e26101652f70a96029c
michlars <michlars@student.chalmers.se>
2022-11-23 16:09:20 +0100

    Connect User Appointments, resolves #9
    Connected functionality allowing users to view their booked appointments

--- src/components/Appointment.js
+++ src/components/Appointment.js
@@ -3,21 +3,30 @@ import "./AppointmentStyles.css";
 import LoginContext from "../contexts/LoginContext";
 import { useContext } from "react";
 
-const Appointment = () => {
-  const { userNum } = useContext(LoginContext);
+
+
+const Appointment = ({appointmentInfo, deleteFunc, editFunc}) => {
+  console.log("appINFO:",appointmentInfo)
+  
+  const issuance = appointmentInfo.issuance
+  const date = appointmentInfo.date
+  const request  = appointmentInfo.request_id
+  const dentist = appointmentInfo.dentist_id[0].first_name + " "+  appointmentInfo.dentist_id[0].last_name
+  const clinic = appointmentInfo.dentist_id[0].works_at[0].name
+  const appointment = appointmentInfo._id
 
   return (
     <>
     <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'></link>
-    <div class="app-box">
+    <div className="app-box">
       <h2>Appointment #123</h2>
-      <p>user id: {userNum}</p>
-      <p>dentist id: </p>
-      <p>request id: </p>
-      <p>appointment id: </p>
-      <p>issuance: </p>
-      <p>date: </p>
-      <p>time: </p>
+      <p>Dentist: {dentist} </p>
+      <p>Clinc: {clinic} </p>
+      <p>Request: {request} </p>
+      <p>Appointment: {appointment} </p>
+      <p>issuance: {issuance} </p>
+      <p>Date: {date} </p>
+      <p>Time: </p>
       <button>delete</button>
       <button>edit</button>
       </div>
--- src/routes/MainPage.js
+++ src/routes/MainPage.js
@@ -2,14 +2,102 @@ import React from "react";
 import Appointment from "../components/Appointment";
 import Map from "../components/Map";
 import "./MainPage.css";
+import { useState } from "react";
+import { useContext } from "react";
+import LoginContext from "../contexts/LoginContext";
+
+
+let clientLoaded = false
+
+const Paho = require('paho-mqtt')
+
+
+const brokerHost = 'localhost'
+const brokerPort = 9001
+const clientId = ""
+
+
+const sQos = 2
+const pQos = 2
+//list of all the appointments that are not connected to the react variable
+let nonReactAppointments = null
+
+let isLoaded = false
+let initLoad = false
+let update = null
+
 
 export default function Home() {
+  const { userNum } = useContext(LoginContext);
+
+  if(!clientLoaded){
+    clientLoaded = true
+    const client = new Paho.Client(brokerHost,brokerPort,clientId)
+
+
+  //handles the appointments that are recieved from the request
+  const onMessage = (message) => {
+    try{
+        const resJSON = JSON.parse(message.payloadString)
+        appointments = resJSON
+        console.log("RES appoint:",appointments)
+        let n = -1
+        
+        nonReactAppointments = appointments.map(appointment => {
+            n++;
+            const info = appointment
+            return <Appointment appointmentInfo={info} key={n} />
+        })
+
+        if(isLoaded){
+            update(nonReactAppointments)
+        } 
+        
+
+    } catch(e){
+        console.log(e)
+    }
+  }
+
+client.onMessageArrived = onMessage;
+
+client.connect({onSuccess: onConnect})
+
+const p_number = userNum
+
+function onConnect () {
+  client.subscribe(`${p_number}/appointments`,{qos:sQos, onSuccess: () => {
+    console.log('user appoint subbed')
+    console.log('pNumber',p_number)
+    const payload = {operation: 'user-appointments', personal_number: p_number, opCat: 'appointment'}
+    const strPayload = JSON.stringify(payload)
+    client.publish(`common/${p_number}`, strPayload,pQos)
+  }})
+}
+  }
+
+
+  let [appointments, setAppointments] = useState([])
+
+   isLoaded = true
+
+    
+  if(nonReactAppointments && !initLoad){
+    setAppointments(nonReactAppointments)
+    initLoad =true
+  }
+    
+  update = (newAppointments) =>{
+    setAppointments(newAppointments)
+  }
+
+
     return (
       <div>
+        {appointments}
         <Map zoom={10} center={{"lat":57.75,"lng":11.92}} />
-        <Appointment />
-        <Appointment />
-        <Appointment />
+        Personal Number: {userNum}
+        
       </div>
     );
   }
