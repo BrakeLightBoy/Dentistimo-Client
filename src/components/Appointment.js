d433db59464b9582a3703b485b1b4885ae8f0a78
michlars <michlars@student.chalmers.se>
2022-11-24 17:06:08 +0100

    Update User & Dentist Appointments
    Updated the appointments to have more appropriate date and time formatting

--- src/components/Appointment.js
+++ src/components/Appointment.js
@@ -9,7 +9,9 @@ const Appointment = ({appointmentInfo, deleteFunc, editFunc}) => {
   console.log("appINFO:",appointmentInfo)
   
   const issuance = appointmentInfo.issuance
-  const date = appointmentInfo.date
+  const date = new Date(appointmentInfo.date)
+  const appointmentDate = date.getDate() + '-' + (date.getMonth()+1) + '-' + date.getFullYear()
+  const appointmentTime = date.getHours() + ':' + date.getMinutes()
   const request  = appointmentInfo.request_id
   const dentist = appointmentInfo.dentist_id[0].first_name + " "+  appointmentInfo.dentist_id[0].last_name
   const clinic = appointmentInfo.dentist_id[0].works_at[0].name
@@ -25,8 +27,8 @@ const Appointment = ({appointmentInfo, deleteFunc, editFunc}) => {
       <p>Request: {request} </p>
       <p>Appointment: {appointment} </p>
       <p>issuance: {issuance} </p>
-      <p>Date: {date} </p>
-      <p>Time: </p>
+      <p>Date: {appointmentDate} </p>
+      <p>Time: {appointmentTime}</p>
       <button>delete</button>
       <button>edit</button>
       </div>
--- src/components/DentistAppointment.js
+++ src/components/DentistAppointment.js
@@ -9,7 +9,9 @@ const DentistAppointment = ({appointmentInfo, deleteFunc, editFunc}) => {
   console.log("appINFO:",appointmentInfo)
   
   const issuance = appointmentInfo.issuance
-  const date = appointmentInfo.date
+  const date = new Date(appointmentInfo.date)
+  const appointmentDate = date.getDate() + '-' + (date.getMonth()+1) + '-' + date.getFullYear()
+  const appointmentTime = date.getHours() + ':' + date.getMinutes()
   const request  = appointmentInfo.request_id
   const clinic = appointmentInfo.dentist_id[0].works_at[0].name
   const patient = appointmentInfo.user_id[0].first_name + ' ' + appointmentInfo.user_id[0].last_name
@@ -25,10 +27,9 @@ const DentistAppointment = ({appointmentInfo, deleteFunc, editFunc}) => {
       <p>Request: {request} </p>
       <p>Appointment: {appointment} </p>
       <p>issuance: {issuance} </p>
-      <p>Date: {date} </p>
-      <p>Time: </p>
+      <p>Date: {appointmentDate}</p>
+      <p>Time: {appointmentTime}</p>
       <button>delete</button>
-      <button>edit</button>
       </div>
     </>
   );
