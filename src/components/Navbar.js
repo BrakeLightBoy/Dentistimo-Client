9742a93d4f8e9892580c45e7affb4531d0f54076
NoahJ <gusnoahjo@student.gu.se>
2022-11-26 20:16:49 +0100

    Fix video to better fit with title contrast, massively changed code structure and complexity for landing page thanks to (source:) Brad Traversy's codepen.  #19

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,78 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import placeholder from "../assets/placeholder.jpg";
-import video from "../assets/landing_page_vid.mp4"
+import video from "../assets/landing_page_vid_v4.mp4";
 import "./NavbarStyles.css";
 
 const NavBar = () => {
   const navigate = useNavigate();
   return (
     <>
-    <section class = "showcase">
-
-    <div class="video-container"><video src={video} autoPlay muted loop></video></div>
-
-    <div class="content">
-      <h1>BOOK DENTISTS ALL OVER GOTHENBURG</h1>
-      <h2 id="projName">PROJECT TITAN</h2>
-      <div id="container">
-      
-        <div class="col deadcol"></div>
-        <div class="col col-1">
-
-          <div id="allBtns">
-            <button
-              id="loginBtn"
-              class="steam-button"
-              onClick={() => {
+      	<section class="showcase" id = "top-page">
+			<div class="video-container">
+				<video src={video} autoPlay muted loop></video>
+			</div>
+			<div class="content">
+				<h1>BOOK DENTISTS ALL OVER GOTHENBURG</h1>
+				<h2 id="projName">PROJECT TITAN</h2>
+				<a href="#about" class="steam-button" onClick={() => {
                 navigate("/login");
-              }}>
-              Log In
-            </button>
+              }}>Log In</a>
+        
+      <a href="#about" class="steam-button">Learn More</a>
+      <a href="#contact-us" class="steam-button">Contact Us</a>
+			</div>
+		</section>
 
-            <div class="buttonsDiv">
-              <button class="steam-button">
-                <a href="#learn-more">Learn More</a>
-              </button>
-              <button class="steam-button">
-                <a href="#contact-us">Contact Us</a>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    
-    </section>
-
-    
-
-      <div id="learn-more" class="hp-section">
-        <h1>Learn More</h1>
-        <p class="basicDesc">
-            Seamless, easy to use, efficient. Just pick a preferred time and we
-            will let you know what's available! Some more sample text to flesh
-            out this description
-          </p>
+<section id="about">
+  <h1>Learn More</h1>
           <p class="basicDesc">
-            "Inspirational relevant quote............" - Famous person
-          </p>
-      </div>
-
-      <div id="contact-us" class="hp-section">
-        <h1>Contact Us</h1>
-      </div>
-
-      <div id="footer">
-        <h1>Footer</h1>
-        <button class="steam-button">
-          <a href="#projName">Back to Top</a>
-        </button>
-      </div>
+          Seamless, easy to use, efficient. Just pick a preferred time and we will let you know what's available! Some more sample text
+        </p>
+  <p class="basicDesc">
+          "Inspirational relevant quote" - Famous person
+        </p>
+</section>
+<section id="contact-us"><h1>Contact Us</h1></section>
+<section id="about">
+  <a href="#top-page" class="steam-button">Back to Top</a>
+</section>
   
-
-      
     </>
   );
 };
