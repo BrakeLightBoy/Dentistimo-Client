a584d705efd5414c835eaafc8e7beac37086adf6
norris <norris@student.chalmers.se>
2022-11-26 18:51:50 +0100

    Add delete user appointment #8
    Added the appointment deletion functionality, so that the user appointment is deleted from the database. Doesn't dynamically delete the appointment on the website and you have to log in again to see the change

--- src/components/Appointment.js
+++ src/components/Appointment.js
@@ -3,9 +3,25 @@ import "./AppointmentStyles.css";
 import LoginContext from "../contexts/LoginContext";
 import { useContext } from "react";
 
+const Paho = require('paho-mqtt')
+
+const brokerHost = 'localhost'
+const brokerPort = 9001
+const clientId = ""
+
+const client = new Paho.Client(brokerHost,brokerPort,clientId)
+const sQos = 2
+const pQos = 2
+
+client.connect({onSuccess: onConnect})
+
+function onConnect () {
+  console.log('CONN SUCC LOGIN')
+}
 
 
 const Appointment = ({appointmentInfo, deleteFunc, editFunc}) => {
+  const { userNum } = useContext(LoginContext);
   console.log("appINFO:",appointmentInfo)
   
   const issuance = appointmentInfo.issuance
@@ -16,6 +32,16 @@ const Appointment = ({appointmentInfo, deleteFunc, editFunc}) => {
   const dentist = appointmentInfo.dentist_id[0].first_name + " "+  appointmentInfo.dentist_id[0].last_name
   const clinic = appointmentInfo.dentist_id[0].works_at[0].name
   const appointment = appointmentInfo._id
+  
+  function deleteFunc() {
+    const payload = {operation: 'delete-user-appointment', request_id:request, opCat: 'appointment'}
+    const strPayload = JSON.stringify(payload)
+    console.log(`common/${userNum}`+ strPayload +' qos:'+ pQos)
+    client.subscribe(`${userNum}/#`,{qos:sQos, onSuccess: () => {
+    client.publish(`common/${userNum}`, strPayload,pQos)
+  }}) 
+  }
+
 
   return (
     <>
@@ -29,7 +55,7 @@ const Appointment = ({appointmentInfo, deleteFunc, editFunc}) => {
       <p>issuance: {issuance} </p>
       <p>Date: {appointmentDate} </p>
       <p>Time: {appointmentTime}</p>
-      <button>delete</button>
+      <button onClick={deleteFunc}>delete</button>
       <button>edit</button>
       </div>
     </>
