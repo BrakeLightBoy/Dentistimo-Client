bc5da09e766d3f87bd0f4cf3962b0e5b2a1ac26c
noahma <noahma@net.chalmers.se>
2022-12-29 19:25:22 +0900

    Move language button from landing page to settings with code still present in both

--- src/routes/Settings.js
+++ src/routes/Settings.js
@@ -1,8 +1,15 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import "./SettingsStyles.css";
 import NavPanel from "../components/NavPanel";
+
+
+const engLang = require('../languages/english').navbar
+const sweLang = require('../languages/swedish').navbar
+let inUse = false
+let chosenLang = localStorage.getItem('lang');
+
 
 const Paho = require('paho-mqtt')
 
@@ -45,6 +52,79 @@ export default function Settings() {
   }}) 
   }
 
+  const navigate = useNavigate();
+  const [pageLang, setLang] = useState('eng'); 
+  
+  const [title, setTitle] = useState(engLang.title);
+  const [loginButtonText, setLoginButtonText] = useState(engLang.loginButtonText);
+  const [aboutButtonText, setAboutButtonText] = useState(engLang.aboutButtonText);
+  const [contactText, setContactText] = useState(engLang.contactText);
+  const [p1Text, setp1Text] = useState(engLang.p1Text);
+  const [p2Text, setp2Text] = useState(engLang.p2Text);
+  const [p3Text, setp3Text] = useState(engLang.p3Text);
+  const [p4Text, setp4Text] = useState(engLang.p4Text);
+  const [quoteText, setQuoteText] = useState(engLang.quoteText);
+  const [contactDescriptionText, setContactDescriptionText] = useState(engLang.contactDescriptionText);
+  const [fullStackText, setFullStackText] = useState(engLang.fullStackText);
+  const [backendText, setBackendText] = useState(engLang.backendText);
+  const [frontendText, setFrontendText] = useState(engLang.frontendText);
+  const [toTopText, setToTopText] = useState(engLang.toTopText);
+  
+
+  function checkLang() {
+    if(chosenLang !== pageLang){
+      
+      setLang(chosenLang)
+      let langObj = null
+      switch (chosenLang) {
+        case 'eng':
+          langObj = engLang  
+          break;
+        case 'swe':
+          langObj = sweLang
+          break;
+        default:
+          langObj = engLang
+          break;
+      }
+
+      setTitle(langObj.title);
+      setLoginButtonText(langObj.loginButtonText);
+      setAboutButtonText(langObj.aboutButtonText);
+      setContactText(langObj.contactText);
+      setp1Text(langObj.p1Text);
+      setp2Text(langObj.p2Text);
+      setp3Text(langObj.p3Text);
+      setp4Text(langObj.p4Text);
+      setQuoteText(langObj.quoteText);
+      setContactDescriptionText(langObj.contactDescriptionText);
+      setFullStackText(langObj.fullStackText);
+      setBackendText(langObj.backendText);
+      setFrontendText(langObj.frontendText);
+      setToTopText(langObj.toTopText);
+      inUse = false
+    }
+  }
+  
+  checkLang()
+
+  function toggleLang() {
+    switch (chosenLang) {
+      case 'eng':
+        localStorage.setItem('lang','swe')
+        chosenLang = 'swe'
+        break;
+      case 'swe':
+        localStorage.setItem('lang','eng')
+        chosenLang = 'eng'
+        break;
+      default:
+        localStorage.setItem('lang','eng')
+        chosenLang = 'eng'
+        break;
+    }
+    checkLang()
+  }
 
   return (
     <>
@@ -59,6 +139,7 @@ export default function Settings() {
     <h2>password</h2>
     <input ref={pass} type="text" placeholder="password"></input>
     <button onClick={saveInfo}>Save</button>
+    <a onClick={toggleLang} className="steam-button">Language Button (not final): {pageLang}</a>
     </>
   );
 };
