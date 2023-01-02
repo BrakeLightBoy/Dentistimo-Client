ce7de0adadfcc203fa0e8dd24515bf4de8ed5732
johnbern <gusberjoid@student.gu.se>
2023-01-02 07:44:25 +0100

    Add popup and simple input validation

--- src/routes/Settings.js
+++ src/routes/Settings.js
@@ -1,8 +1,9 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import "./SettingsStyles.css";
 import NavPanel from "../components/NavPanel";
+import Popup from "../components/Popup";
 
 const Paho = require('paho-mqtt')
 
@@ -28,6 +29,41 @@ export default function Settings() {
   const last = useRef(null);
   const pass = useRef(null);
   
+  const [errorMsg, setErrorMsg] = useState(false);
+  const [popupMsg, setPopupMsg] = useState('')
+
+  function validateInfo() {
+    setPopupMsg('')
+    //Probably change popup to highlight the fields that are invalid and only show popup on successful changes
+    /*if (!((/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.current.value))) && email.current.length > 0) {
+      setPopupMsg('Invalid email format')
+      setErrorMsg(true)
+    }
+    */
+    if (pass.current.value.length > 0 && pass.current.value.length < 5) {
+      setPopupMsg('Password must be at least 5 characters long')
+      setErrorMsg(true)
+      return
+    }
+    if (email.current.value.length > 0 ||
+        first.current.value.length > 0 ||
+        last.current.value.length > 0 ||
+        pass.current.value.length > 0) {
+      saveInfo()
+      setPopupMsg('Updated your info')
+      setErrorMsg(true)
+      return
+    }  
+    if (email.current.value.length === 0 &&
+        first.current.value.length === 0 &&
+        last.current.value.length === 0 &&
+        pass.current.value.length ===  0) {
+      setPopupMsg('All fields are empty')
+      setErrorMsg(true)
+      return
+    } 
+  }
+
   function saveInfo() {
     const payload = {
       operation: 'modify',
@@ -42,13 +78,14 @@ export default function Settings() {
     console.log(`common/${uID}`+ strPayload +' qos:'+ pQos)
     client.subscribe(`${uID}/#`,{qos:sQos, onSuccess: () => {
     client.publish(`common/${uID}`, strPayload,pQos)
-  }}) 
+  }})
   }
 
 
   return (
     <>
     <NavPanel></NavPanel>
+    <Popup trigger={errorMsg} setTrigger={setErrorMsg}><p>{popupMsg}</p></Popup>
     <h1>Settings</h1>
     <h2>Email Address</h2>
     <input ref={email} type="text" placeholder="Email"></input>
@@ -58,7 +95,7 @@ export default function Settings() {
     <input ref={last} type="text" placeholder="last name"></input>
     <h2>password</h2>
     <input ref={pass} type="text" placeholder="password"></input>
-    <button onClick={saveInfo}>Save</button>
+    <button onClick={validateInfo}>Save</button>
     </>
   );
 };
