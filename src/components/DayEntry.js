32392d5dc33c80467cfaed7afe53416e505c6b82
wojpec <guspecwo@student.gu.se>
2023-01-08 20:29:05 +0100

    Remove dead code

--- src/components/DayEntry.js
+++ src/components/DayEntry.js
@@ -60,26 +60,6 @@ const DayEntry = ({day, appointments, monthDisplayed, yearDisplayed, isDummy}) =
                 </div>
             )
     }
-
-    // return (displayFunction()) ? (
-    //     <div>
-    //         <div className="DayGrid">
-    //         <div>Day {day}</div>
-    //         </div>
-    //         <div className="AppointmentBox">
-    //             {appointments}
-    //         </div>
-    //     </div>
-    // ) : (
-    //     <div>
-    //         <div className="DayGrid">
-    //         <button className="AppointmentDay">Day {day}</button>
-    //         </div>
-    //         <button className="AppointmentBox">
-    //         <div>NO APPOINTMENTS</div>
-    //         </button>
-    //     </div>
-    // )
 }
 
 export default DayEntry
